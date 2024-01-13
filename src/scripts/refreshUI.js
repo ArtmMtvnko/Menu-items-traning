b9089e96f270cc9343c0df6a8bc0eb049b412c1a
Artem <artemm3note@gmail.com>
2024-01-13 20:20:34 +0200

    Added html and css for redio button to switch celsius to fahrenheit and back. (it is just decor)

--- src/scripts/refreshUI.js
+++ src/scripts/refreshUI.js
@@ -1,7 +1,10 @@
 const temperature = document.querySelector('.temperature__number')
 const temperatureInfo = document.querySelector('.temperature__info')
+const sunstate = document.querySelector('.temperature__sunstate')
 
 export function refreshDataInUI(data) {
     temperature.innerText = data.current.temp_c + '℃'
     temperatureInfo.children[0].innerText = `${data.location.name}, ${data.location.country}`
+    sunstate.children[0].innerText = 'Sunrise: ' + data.forecast.forecastday[0].astro.sunrise
+    sunstate.children[1].innerText = 'Sunset: ' + data.forecast.forecastday[0].astro.sunset
 }
