25a6fc53d6bc615db8e030b782d070dec4d87a39
Artem <artemm3note@gmail.com>
2024-01-15 01:16:33 +0200

    Added icon for main weather temperature.

--- src/scripts/forecastCards.js
+++ src/scripts/forecastCards.js
@@ -2,15 +2,27 @@ const forecastCards = document.querySelectorAll('.forecast__day')
 
 const celsiusRadioBtn = document.querySelector('#celsius')
 
+const daysOfWeek = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Fryday', 'Saturday',
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Fryday', 'Saturday'
+]
+
+const currentDayNumber = new Date().getDay()
+console.log(currentDayNumber)
+
 export function refreshCards(data) {
     const days = data.forecast.forecastday
     if (celsiusRadioBtn.checked) {
         for (let i = 0; i < 6; i++) {
-            forecastCards[i].children[1].innerText = days[i].day.avgtemp_c + '℃'
+            forecastCards[i].children[0].src = days[i + 1].day.condition.icon
+            forecastCards[i].children[1].innerText = days[i + 1].day.avgtemp_c + '℃'
+            forecastCards[i].children[2].innerText = daysOfWeek[currentDayNumber + i + 1]
         }
     } else {
         for (let i = 0; i < 7; i++) {
-            forecastCards[i].children[1].innerText = days[i].day.avgtemp_f + '°F'
+            forecastCards[i].children[0].src = days[i + 1].day.condition.icon
+            forecastCards[i].children[1].innerText = days[i + 1].day.avgtemp_f + '°F'
+            forecastCards[i].children[2].innerText = daysOfWeek[currentDayNumber + i + 1]
         }
     }
 }
--- src/scripts/refreshUI.js
+++ src/scripts/refreshUI.js
@@ -3,6 +3,7 @@ import { getTemperature } from "./measurement"
 export const temperature = document.querySelector('.temperature__number')
 const temperatureInfo = document.querySelector('.temperature__info')
 const sunstate = document.querySelector('.temperature__sunstate')
+const mainWeatherIcon = document.querySelector('#mainWeatherIcon')
 
 export function refreshDataInUI(data) {
     // temperature.innerText = data.current.temp_c + '℃'
@@ -10,4 +11,5 @@ export function refreshDataInUI(data) {
     temperatureInfo.children[0].innerText = `${data.location.name}, ${data.location.country}`
     sunstate.children[0].innerText = 'Sunrise: ' + data.forecast.forecastday[0].astro.sunrise
     sunstate.children[1].innerText = 'Sunset: ' + data.forecast.forecastday[0].astro.sunset
+    mainWeatherIcon.src = data.current.condition.icon
 }
